6e7f6e4f7b83c97825bd151172d278b3aa417667
Arie_Azzi <ariel_azzi@hotmail.com>
2016-10-27 21:01:10 -0200

    Melhorando o visual com bootstrap, e retornando erro para o usuario

--- loja/produto-formulario.php
+++ loja/produto-formulario.php
@@ -1,9 +1,21 @@
 <?php include("cabecalho.php"); ?>
     <form action="adiciona-produto.php">
-        Nome: <input type="text" name="nome" /><br/>
-        Preço: <input type="number" name="preco" /><br/>
+    	<table class="table">
+			<tr>
+				<td>Nome</td>
+				<td><input class="form-control" type="text" name="nome" /></td>
+			</tr>
+			<tr>
+				<td>Preço</td>
+				<td><input class="form-control" type="number" name="preco" /></td>
+			</tr>
+			<tr>
+				<td>
+        			<input class="btn btn-primary" type="submit" value="Cadastrar" />
+				</td>
+			</tr>
+    	</table>
 
-        <input type="submit" value="Cadastrar" />
     </form>
 </html>
 <?php include("rodape.php"); ?>
